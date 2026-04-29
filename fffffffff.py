a25148778bbb1da50a4fd2db9600e738be81cca8
mivalem <mivalem@inbox.ru>
2026-04-30 02:32:10 +0300

    Update fffffffff.py

--- fffffffff.py
+++ fffffffff.py
@@ -21,6 +21,6 @@ def home():
     return redirect(REDIRECT_URL)
 
 
-if name == "__main__":
+if __name__ == "__main__":
     port=int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
